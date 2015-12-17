d28e6eedef66d853858ff1d644ef3112c474bc07
iapostol <ionut.apostol@softvision.ro>
2015-12-17 14:16:34 +0200

    Updated solutions

--- AmazonDroneSimulator/Extensions.cs
+++ AmazonDroneSimulator/Extensions.cs
@@ -14,7 +14,7 @@ namespace AmazonDroneSimulator
 
         public static void Refresh(this UIElement uiElement)
         {
-            uiElement.Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
+            uiElement.Dispatcher.Invoke(DispatcherPriority.Background, EmptyDelegate);
         }
     }
 }
--- DroneCodeGenerator/MainWindow.xaml.cs
+++ DroneCodeGenerator/MainWindow.xaml.cs
@@ -29,41 +29,47 @@ namespace DroneCodeGenerator
         {
             var lines = txt.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).Where(line => !String.IsNullOrEmpty(line) && !String.IsNullOrWhiteSpace(line)).ToList();
 
+            var ln1 = lines.Select(line => line.Split(' ')[0]).ToList();
+            var ln2 = lines.Select(line => line.Split(' ')[1]).ToList();
 
-            var execTimes = lines.Select(line => line.Split(' ')[0]).ToList();
+            var execTimes = new List<int>();
 
-            var commands = lines.Select(line => line.Split(' ')[1]);
+            var commands = new List<int>();
+
+            for (var index1 = 0; index1 < ln1.Count; index1++)
+            {
+                if (index1 == 0)
+                {
+                    execTimes.Add(Convert.ToInt32(ln1[index1]) + 1);
+                }
+                else
+                {
+                    execTimes.Add(Convert.ToInt32(ln1[index1]));
+                }
+                commands.Add(Convert.ToInt32(ln2[index1]));
+            }
+
 
             StringBuilder builder = new StringBuilder();
 
             builder.AppendLine("LDA [1000]");
-
-            var jgeIndex = execTimes.Count()*2 + 1;
-            execTimes[0] = (Convert.ToInt32(execTimes[0]) - 1).ToString();
-            foreach (var exec in execTimes)
-            {
-                builder.AppendLine("ADDA " + exec)
-                    
-                    .AppendLine("JGE " + jgeIndex);
-                jgeIndex += 2;
-            }
-            jgeIndex -= 1;
-            int count = 0;
-            foreach (var cmd in commands)
-            {
-                count++;
-                builder.AppendLine("LDA " + cmd);
-                if(count != commands.Count())
-                {
-                    builder.AppendLine("JGE " + jgeIndex);
-                }
-            }
-
-            builder.AppendLine("STA [0]");
-            builder.AppendLine("LDA [1000]");
-            builder.AppendLine("SUBA 1");
+            builder.AppendLine("ADDA 1");
             builder.AppendLine("STA [1000]");
+            int nextInstr = 8;
+            for (int index = 0; index < execTimes.Count - 1; index++)
+            {
+                builder.AppendLine("SUBA " + (execTimes[index]));
+                builder.AppendLine("JGE " + nextInstr);
+                builder.AppendLine("LDA " + commands[index]);
+                builder.AppendLine("STA [0]");
+                builder.AppendLine("HLT");
+                nextInstr += 5;
+            }
+            var indexLast = execTimes.Count - 1;
+            builder.AppendLine("LDA " + commands[indexLast]);
+            builder.AppendLine("STA [0]");
             builder.AppendLine("HLT");
+
 
 
             code.Text = builder.ToString();
@@ -75,3 +81,34 @@ namespace DroneCodeGenerator
 //SUBA 1
 //STA[1000]
 //HLT
+
+
+
+//builder.AppendLine("LDA [1000]");
+
+//            var jgeIndex = execTimes.Count() * 2 + 1;
+//execTimes[0] = (Convert.ToInt32(execTimes[0]) - 1).ToString();
+//            foreach (var exec in execTimes)
+//            {
+//                builder.AppendLine("ADDA " + exec)
+
+//                    .AppendLine("JGE " + jgeIndex);
+//jgeIndex += 2;
+//            }
+//            jgeIndex -= 1;
+//            int count = 0;
+//            foreach (var cmd in commands)
+//            {
+//                count++;
+//                builder.AppendLine("LDA " + cmd);
+//                if(count != commands.Count())
+//                {
+//                    builder.AppendLine("JGE " + jgeIndex);
+//                }
+//            }
+
+//            builder.AppendLine("STA [0]");
+//            builder.AppendLine("LDA [1000]");
+//            builder.AppendLine("SUBA 1");
+//            builder.AppendLine("STA [1000]");
+//            builder.AppendLine("HLT");
